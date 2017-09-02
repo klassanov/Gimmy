bb5ffb9726bc71d61c8f57054c481e44e3dda4fd
klassanov <klassanov@gmail.com>
2017-09-02 16:56:21 +0200

    First commit, Just testing!

---
+++ XMLTransformer/XMLTransformer/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLTransformer
+{
+    public class Test
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
